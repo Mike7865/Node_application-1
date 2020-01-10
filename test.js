a1f6043651f0f86f877eb6ec7a37a26613b711e4
Mike38 <mshar50055@gmail.com>
2020-01-10 19:00:53 +0300

    test.js improved

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-const yargs = require('yargs');
-const path = require('path');
-const fs = require('fs');
+const yargs = require('yargs')
+const path = require('path')
+const fs = require('fs')
 const paths = { source: null, dist: null }
 
 const argv = yargs
@@ -28,47 +28,67 @@ const argv = yargs
   .epilog('First homework on the node.js course')
   .argv
 
-console.log(argv.entry, argv.output);
+console.log(argv.entry, argv.output)
 
-paths.source = path.normalize(path.join(__dirname, argv.entry));
-paths.dist = path.normalize(path.join(__dirname, argv.output));
+paths.source = path.normalize(path.join(__dirname, argv.entry))
+paths.dist = path.normalize(path.join(__dirname, argv.output))
+
+if (!fs.existsSync('./Folder_2')) {
+  fs.mkdirSync('./Folder_2')
+}
+
+fs.copyFile(folder, err => {
+  console.error(err)
+})
+
+function startCopyFile (_dir) {
+  fs.readdir(_dir, function (err, items) {
+    for (let i = 0; i < items.length; i++) {
+      if (fs.lstatSync(_dir + '/' + items[i]).isDirectory()) {
+        startCopyFile(_dir + '/' + items[i])
+      } else {
+        fs.copyFile(_dir + '/' + items[i], `./sss/ddd/${items[i]}`)
+      }
+    }
+  })
+}
 
 const sortFiles = (src) => {
   fs.readdir(src, (error, files) => {
     if (error) {
-      process.exit(500);
+      process.exit(500)
     }
 
     if (!files.length) {
-      process.exit(404);
+      process.exit(404)
     }
 
     for (let index = 0; index < files.length; index++) {
-      const currentUrl = path.join(src, files[index]);
+      const currentUrl = path.join(src, files[index])
       fs.stat(currentUrl, (error, state) => {
-          if (state.isDirectory()) {
-            sortFiles(currentUrl);
-          }   else {
-            console.info(currentUrl);
-            console.log(" ");
-          }
-      });
+        if (state.isDirectory()) {
+          sortFiles(currentUrl)
+        } else {
+          console.info(currentUrl)
+          console.log("")
+        }
+      })
     }
-  });
+  })
 }
 
-sortFiles(paths.source);
+sortFiles(paths.source)
 
 process.on('exit', code => {
   switch (code) {
     case 500:
-      console.error('directory read failed');
-      break;
+      console.error('directory read failed')
+      break
     case 404:
-      console.error('Directory is clear.');
-      break;
+      console.error('Directory is clear.')
+      break
     default:
       console.log('done')
-      break;
+      break
   }
-});
+})
