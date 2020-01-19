49db79722c8d128669be969dec49780331a353f4
Mike38 <mshar50055@gmail.com>
2020-01-19 22:32:46 +0300

    server.js modified

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+const http = require("http")
+
+http.createServer(function(request, response){
+    console.log("Url: " + request.url);
+    console.log("GET" + request.method);
+    console.log("User-Agent: " + request.headers["user-agent"]);
+    console.log("Все заголовки");
+    console.log(request.headers);
+     
+    response.end();
+}).listen(3000);
+
 var date = new Date(2020, 0, 19, 21, 30, 0)
 
 var options = {
@@ -12,8 +24,8 @@ var options = {
   second: 'numeric'
 }
 
-var t = new Date();
-console.log(t.toTimeString());
+var t = new Date()
+console.log(t.toTimeString())
 
 console.log(date.toLocaleString('ru', options))
 console.log(date.toLocaleString('en-US', options))
