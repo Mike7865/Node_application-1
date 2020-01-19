37da5e3ae49ba70af6b304811ef97048b7e06a3b
Mike38 <mshar50055@gmail.com>
2020-01-19 22:45:50 +0300

    observer.js modified

--- server.js
+++ server.js
@@ -1,31 +1,14 @@
 const http = require("http")
 
 http.createServer(function(request, response){
-    console.log("Url: " + request.url);
-    console.log("GET" + request.method);
-    console.log("User-Agent: " + request.headers["user-agent"]);
-    console.log("Все заголовки");
-    console.log(request.headers);
+    console.log("Url: " + request.url)
+    console.log("GET" + request.method)
+    console.log("User-Agent: " + request.headers["user-agent"])
+    console.log("Все заголовки")
+    console.log(request.headers)
      
-    response.end();
-}).listen(3000);
-
-var date = new Date(2020, 0, 19, 21, 30, 0)
-
-var options = {
-  era: 'long',
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric',
-  weekday: 'long',
-  timezone: 'UTC',
-  hour: 'numeric',
-  minute: 'numeric',
-  second: 'numeric'
-}
+    response.end()
+}).listen(3000)
 
 var t = new Date()
 console.log(t.toTimeString())
-
-console.log(date.toLocaleString('ru', options))
-console.log(date.toLocaleString('en-US', options))
